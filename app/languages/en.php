3b2e2f9624cc70047cf98b8f96204c79d0429c4f
Dumra <d.shuliakov@gmail.com>
2015-04-10 12:23:07 +0300

    update

---
+++ app/languages/en.php
@@ -0,0 +1,17 @@
+<?php
+
+$messages = array(
+    "sign in"    => "Sign in",
+    "welcome"   => "Welcome to our online store!",
+    "cart"      => "My Cart",
+    "home"      => "Home",
+    "add"       => "Add to cart",
+    "filters"   => "Filters",
+    "lan"       => "Languages",
+    "authors"   =>  "Authors",
+    "item"      => "item(-s)",
+    "sign out"  => "Sign out",
+    "history"   => "History orders",
+    "hello"     => "Hello",
+    "back"      => "Back",
+);
